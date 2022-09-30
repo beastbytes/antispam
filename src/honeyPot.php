4679e47b8518225c0888923912609295d16e396f
Chris Yates <beastbytes@users.noreply.github.com>
2022-09-30 13:56:53 +0100

    Update honeyPot.php

--- src/honeyPot.php
+++ src/honeyPot.php
@@ -1,1 +1,41 @@
+<php
 
+/**
+ * Honey pots are attributes that span bots are likely
+ * to complete, for example 'email'.
+ * Each attribute creates two input fields:
+ * 1. A hidden field with the original attribute name.
+ * This should be empty on form submission;
+ * if not a spam bot has completed it.
+ * 2. A text input with a new name that is shown to the user.
+ */
+class HoneyPot
+{
+
+    /**
+     * @var array $honetPots List of honey pot attributes
+     * Examples: ['name', 'email']
+     */
+    private array $honeyPots = [];
+
+    public function addHoneyPots(string $honeyPot): self)
+    {
+        $this->HoneyPots[] = $this->HoneyPot;
+        return $this;
+    }
+
+    public function withHoneyPots(array $honeyPots): self)
+    {
+        $this->HoneyPots = $this->HoneyPots;
+        return $this;
+    }
+
+    public function getHoneyPots()
+    {
+        $honeyPots = [];
+        foreach ($this->HoneyPots as $honeyPot) {
+            $honeyPots[$honeyPot] = md5($honeyPot);
+        }
+        return $honeyPots;
+    }
+}
